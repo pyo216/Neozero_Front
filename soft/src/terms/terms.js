5015ebc7d0915bdc68c816a88f33f2ba4fb5605d
pyo216 <147482854+pyo216@users.noreply.github.com>
2025-01-08 16:00:12 +0900

    Update terms.js

--- soft/src/terms/terms.js
+++ soft/src/terms/terms.js
@@ -4,11 +4,11 @@ import { useNavigate } from 'react-router-dom'; // ページ遷移用
 import styles from './login.module.css'; // CSSモジュール(cssファイルかく)
 */
 
-const Terms = () => {
+const Post = () => {
     //ここから下変える
   const navigate = useNavigate(); // ページ遷移用
 return (
       <div>広告</div>
     );
 };
-export default Terms;
+export default Post;
