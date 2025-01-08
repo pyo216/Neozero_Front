aff945ac0590e6d25163da17ef9f12daf2f235e9
Yuha1022 <147596185+Yuha1022@users.noreply.github.com>
2025-01-08 15:54:03 +0900

    Update account_reg.js

--- soft/src/account_reg/account_reg.js
+++ soft/src/account_reg/account_reg.js
@@ -8,68 +8,10 @@ const AccountReg = () => {
     //ここから下変える
   const navigate = useNavigate(); // ページ遷移用
 
-  const handleLogin = () => { // 「ログイン」ボタン押下
-    navigate('/top'); // トップページに移動
-  };
-
-  const handleRegister = () => { //「新規会員登録」ボタン押下
-    navigate('/terms'); // 新規会員登録ページに移動
-  };
 
   return (
-    <div className={styles.body}>
-      <img
-        src={pawloversImg} // PawLovers画像
-        alt="pawloversImg" // 代替テキスト
-        className={styles.pawloversImg}
-      />
-
-      <form className={styles.login}>
-
-        <div className={styles.formGroup}>
-          <label htmlFor="email">メールアドレス</label>
-          <input // メールアドレス入力欄
-            type="email"
-            id="email"
-            placeholder="メールアドレスを入力してください"
-            required
-          />
-        </div>
-
-        <div className={styles.formGroup}>
-          <label htmlFor="password">パスワード</label>
-          <input // パスワード入力欄
-            type="password"
-            id="password"
-            placeholder="パスワードを入力してください"
-            required
-          />
-        </div>
-
-        <p className={styles.errorMessage}></p>  
-
-        <button // 「ログイン」ボタン
-          type="button"
-          onClick={handleLogin}
-          className={styles.loginButton}
-        >
-          ログイン
-        </button>
-
-        <p className={styles.new}>・・・・・・・・・・・・・・・・・初めての方はこちら・・・・・・・・・・・・・・・・・・</p>
-
-        <button //「新規会員登録」ボタン
-          type="button"
-          onClick={handleRegister}
-          className={styles.registerButton}
-        >
-          新規会員登録
-        </button>
-
-      </form>
-
-    </div>
-  );
+      <div>広告</div>
+    );
 };
 
 export default AccountReg;
