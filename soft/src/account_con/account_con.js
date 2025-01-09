2ffef523133e0339634b32874e3a9cea60f483e8
(・ー・) <yuha-kut@noMacBook-Pro.local>
2025-01-09 13:34:21 +0900

    アカウント確認

--- soft/src/account_con/account_con.js
+++ soft/src/account_con/account_con.js
@@ -1,6 +1,6 @@
 import React from 'react'; // React用
 import { useNavigate } from 'react-router-dom'; // ページ遷移用
-import styles from './account_con.module.css'; // CSSモジュール(cssファイルかく)
+import styles from './account_con.module.css'; // CSSモジュール
 
 const AccountCon = () => {
   const navigate = useNavigate(); // ページ遷移用
