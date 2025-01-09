fae5579435a41b093fdf5fc0fabf160775d72ad0
284ienaka <260284s@ugs.kochi-tech.ac.jp>
2025-01-09 19:09:41 +0900

    ログアウト画面の追加

--- soft/src/App.js
+++ soft/src/App.js
@@ -26,7 +26,7 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-        <Route path="/" element={<LoginPage />} /> {/* 初期表示でログインページ */}
+        <Route path="/" element={<Logout/>} /> {/* 初期表示でログインページ */}
           <Route path="/login" element={<LoginPage />} /> {/* ログインページ */}
           <Route path="/top" element={<TopPage />} /> {/* トップページ */}
           <Route path="/mypage" element={<MyPage />} /> {/*マイページ */}
