e44a1cd663296049c0740b06368767dff539f2c9
284ienaka <260284s@ugs.kochi-tech.ac.jp>
2025-01-09 17:27:59 +0900

    現在の作業を一時的に保存

--- soft/src/logout/logout.js
+++ soft/src/logout/logout.js
@@ -1,16 +1,44 @@
+//ログアウト画面
 import React from 'react'; // React用
 import { useNavigate } from 'react-router-dom'; // ページ遷移用
-/*
-import styles from './login.module.css'; // CSSモジュール(cssファイルかく)
-*/
+import styles from './logout.module.css'; // CSSモジュール(cssファイルかく)
 
 const Logout = () => {
-    //ここから下変える
   const navigate = useNavigate(); // ページ遷移用
+
+  const handleTop = () => { //「トップページ」ボタン押下 
+    navigate('/top'); // トップページ
+  };
+
+  const handleok = () => { //「 はい」ボタン押下 
+    navigate('/login'); // はいのページ
+  };
+
+  const handleno = () => { //「いいえ」ボタン押下 
+    navigate('/member'); // いいえ
+  };
 
 
   return (
-      <div>広告</div>
+    <div className={styles.body}>
+    <div class="left">
+      <button className={styles.topButton} onClick={handleTop}>トップページへ戻る</button>
+      <div className={styles.advertisement}>広告</div> 
+    </div>
+
+    <div class="center">
+      <div className={styles.logout}>ログアウト</div>
+      <div id="white">
+        <p>ログアウトするワン？</p>
+        <button className={styles.ok} onClick={handleok}>はい</button>
+        <button className={styles.no} onClik={handleno}>いいえ</button>
+      </div>
+    </div>
+
+    <div class="right">
+    <div className={styles.advertisement2}>広告</div>
+    </div>
+  </div>
     );
 };
 
