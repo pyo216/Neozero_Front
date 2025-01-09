7537d9e5915bcce3c8eaa386c2f72b57f505be04
(・ー・) <yuha-kut@noMacBook-Pro.local>
2025-01-09 11:10:40 +0900

    nyakama追加したよーん

--- soft/src/nyakama/nyakama.js
+++ soft/src/nyakama/nyakama.js
@@ -1,14 +1,38 @@
 import React from 'react'; // React用
 import { useNavigate } from 'react-router-dom'; // ページ遷移用
-/*
-import styles from './login.module.css'; // CSSモジュール(cssファイルかく)
-*/
+import styles from './nyakama.module.css'; // CSSモジュール(cssファイルかく)
 
 const Nyakama = () => {
   const navigate = useNavigate(); // ページ遷移用
-//ここから下変える
+
+  const handleTop = () => { // 「トップページに戻る」ボタン押下
+    navigate('/top'); // トップページに移動
+  };
+
  return (
-      <div>広告</div>
+  <div className={styles.body}>
+
+  <div className={styles.left}>
+    <button // 「トップページへ戻る」ボタン
+          className={styles.topButton}
+          onClick={handleTop}
+        >
+          トップページへ戻る
+        </button>
+
+    <div className={styles.advertisement}>広告</div>
+  </div>
+
+  <div className={styles.center}>
+    <div className={styles.title}>nyakama</div>
+    <div className={styles.media}></div>
+  </div>
+
+  <div className={styles.right}>
+    <div className={styles.advertisement2}>広告</div>
+  </div>
+</div>
+
     );
 }
 export default Nyakama;
