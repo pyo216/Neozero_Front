2b0ef940f43d5d7fd6d38ddf72ef455edff8d3f9
284ienaka <260284s@ugs.kochi-tech.ac.jp>
2025-01-10 21:27:30 +0900

    ペット変更確認画面の修正

--- soft/src/pet_con/pet_con.js
+++ soft/src/pet_con/pet_con.js
@@ -1,15 +1,64 @@
+//ペット変更確認画面
 import React from 'react'; // React用
 import { useNavigate } from 'react-router-dom'; // ページ遷移用
-/*
-import styles from './login.module.css'; // CSSモジュール(cssファイルかく)
-*/
+import styles from './pet_con.module.css'; // CSSモジュール(cssファイルかく)
 
 const PetCon = () => {
     //ここから下変える
   const navigate = useNavigate(); // ページ遷移用
 
+  const handleTop = () => { // 「トップページへ戻る」ボタン押下
+    navigate('/top'); // トップページに移動
+  };
+
+  const handleok = () => { // 「OK」ボタン押下
+    navigate('/pet'); // OKページに移動
+  };
+
+  const handleno = () => { // 「NO」ボタン押下
+    navigate('/pet'); // NOページに移動
+  };
+
 return (
-      <div>広告</div>
+  <div className={styles.body}>
+  <div className={styles.left}>
+    <button 
+      className={styles.topbutton} 
+      onclick={handleTop}
+    >
+      トップページへ戻る
+    </button>
+
+    <div className={styles.advertisement}>広告</div>
+  </div>
+
+  <div className={styles.enter}>
+    <div className={styles.account}>ペット情報変更</div>
+    <div className={styles.white}>
+      <p className={styles.con}>KIMIの情報を<br/>
+          本当に変更<br/>
+          していいニャン？<br/>
+      </p>
+      <button 
+        className={styles.okbutton} 
+        onclick={handleok}
+      >
+        変更を確定
+      </button>
+
+      <button 
+        className={styles.nobutton}
+        onclick={handleno}
+      >
+        いいえ
+      </button>
+    </div>
+  </div>
+
+  <div className={styles.right}>
+    <div className={styles.advertisement2}>広告</div>
+  </div>
+</div>
     );
 }
 
