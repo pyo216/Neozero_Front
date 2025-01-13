6e820b57275557b48e690d021a30ed8af5176867
佐川茉椰 <maya@sagawanoMacBook-Pro.local>
2025-01-13 14:17:21 +0900

    やったぞい

--- soft/src/deletion/deletion.js
+++ soft/src/deletion/deletion.js
@@ -1,16 +1,49 @@
+//投稿消去画面
 import React from 'react'; // React用
 import { useNavigate } from 'react-router-dom'; // ページ遷移用
-/*
-import styles from './login.module.css'; // CSSモジュール(cssファイルかく)
-*/
+import styles from './deletion.module.css'; // CSSモジュール(cssファイルかく)
 
 const Deletion = () => {
     //ここから下変える
   const navigate = useNavigate(); // ページ遷移用
 
+  const handletop = () => { //「トップページ」ボタン押下 
+    navigate('/top'); // トップページに移動
+  };
+
+  const handledeletion = () => { //「消去」ボタン押下 
+    navigate('/deletion'); // 投稿消去画面に移動
+  };
+
 
   return (
-      <div>広告</div>
+    <div className={styles.body}>
+    <div className={styles.left}>
+    <button // 「トップページへ戻る」ボタン
+          className={styles.button}
+          onClick={handletop}
+>
+ トップページへ戻る
+</button>
+      <div className={styles.advertisement}>広告</div>
+    </div>
+
+    <div className={styles.center}>
+      <div className={styles.delete}>投稿を消去して<br/>
+      いいワン？
+      </div>
+      <button // 「トップページへ戻る」ボタン
+          className={styles.ok}
+          onClick={handledeletion}
+>
+ 消去
+</button>
+    </div>
+
+    <div className={styles.right}>
+      <div className={styles.advertisement2}>広告</div>
+    </div>
+  </div>
     );
 };
 
