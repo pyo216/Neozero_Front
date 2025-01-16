749ad631229e7e7d5eea2df8f0788ab6d7ff3028
(・ー・) <yuha-kut@noMacBook-Pro.local>
2025-01-16 21:24:10 +0900

    エラー画面の語尾追加

--- soft/src/login/login.js
+++ soft/src/login/login.js
@@ -35,15 +35,15 @@ const Login = () => {
 
   const handleLogin = async () => {
     if (!email) { // メールアドレスが未入力のとき
-      setErrorMessage('メールアドレスを入力してくださいニャン');
+      setErrorMessage('※メールアドレスを入力してくださいニャン。');
       return;
     } else if (!password) { // パスワードが未入力のとき
-      setErrorMessage('パスワードを入力してくださいワン');
+      setErrorMessage('※パスワードを入力してくださいワン。');
       return;
     }
 
     if (!validatePassword(password)) {
-      setErrorMessage('パスワードは半角英数字8～16文字で入力してくださいニャン');
+      setErrorMessage('※パスワードは半角英数字8～16文字で入力してくださいニャン。');
       return;
     }
     //処理とおさない用
@@ -67,13 +67,13 @@ const Login = () => {
           setErrorMessage('');
           navigate('/top');
         } else {  // 認証失敗
-          setErrorMessage('入力情報が間違っていますワン');
+          setErrorMessage('※入力情報が間違っていますワン。');
         }
       } else {
-        setErrorMessage(data.error || 'ログインに失敗しましたニャン');
+        setErrorMessage(data.error || '※ログインに失敗しましたニャン。');
       }
     } catch (error) {
-      setErrorMessage('サーバーとの通信に失敗しましたワン');
+      setErrorMessage('※サーバーとの通信に失敗しましたワン。');
     }
   };
 
