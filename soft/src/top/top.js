72c8d4d8515cbda0ab29f94905598ca946878d71
pyo216 <260306f@ugs.kochi-tech.ac.jp>
2025-01-17 10:26:42 +0900

    ループ文にした

--- soft/src/top/top.js
+++ soft/src/top/top.js
@@ -28,44 +28,47 @@ const TopPage = () => {
     fontFamily: 'CraftMincho, serif'
   };
 
+//配列の作成
+//配列の長さだけ投稿を表示
+const posts = Array.from({ length: 10 }, (_, i) => i + 1); // 投稿データ（1～10の番号）
+return (
+  <div className={fontstyles.fontFamily}>
+    <div className={styles.body}>
+      <div className={styles.above}>
+        <img
+          src={pawloverslogoImg} // PawLoversロゴ画像
+          alt="pawloverslogoImg" // 代替テキスト
+          className={styles.pawloverslogoImg}
+        />
 
-  return (
-    <div className={fontstyles.fontFamily}>
-      <div className={styles.body}>
+        <button
+          className={styles.postButton}
+          onClick={handlePost}
+          style={inputStyle}
+        >
+          投稿する
+        </button>
 
-        <div className={styles.above}>
+        <button
+          className={styles.mypageButton}
+          onClick={handleMypage}
+          style={inputStyle}
+        >
+          マイページ
+        </button>
+      </div>
+
+      <div className={styles.bottom}>
+        <div className={styles.advertisement}>
           <img
-            src={pawloverslogoImg} // PawLoversロゴ画像
-            alt="pawloverslogoImg" // 代替テキスト
-            className={styles.pawloverslogoImg}
+            src={sampleTopImg} // 広告サンプル
+            alt="sampleTopImg" // 代替テキスト
           />
-
-          <button // 「投稿する」ボタン
-            className={styles.postButton}
-            onClick={handlePost}
-            style={inputStyle}
-          >
-            投稿する
-          </button>
-
-          <button // 「マイページ」ボタン
-            className={styles.mypageButton}
-            onClick={handleMypage}
-            style={inputStyle}
-          >
-            マイページ
-          </button>
         </div>
 
-        <div className={styles.bottom}>
-          <div className={styles.advertisement}>
-            <img
-              src={sampleTopImg} // 広告サンプル
-              alt="sampleTopImg" // 代替テキスト
-            />
-          </div>
-          <div className={styles.media}>
-            <div className={styles.white}>
+        <div className={styles.media}>
+          {posts.map((post, index) => (
+            <div key={index} className={styles.white}>
               <div className={styles.post}>
                 <div className={styles.picture}>画像</div>
                 <div className={styles.info}>
@@ -91,75 +94,19 @@ const TopPage = () => {
                 </div>
               </div>
             </div>
-
-            <div className={styles.white}>
-              <div className={styles.post}>
-                <div className={styles.picture}>画像</div>
-                <div className={styles.info}>
-                  <button
-                    className={styles.followButton}
-                    onClick={handleFollow}
-                    style={inputStyle}
-                  >
-                    フォロー
-                  </button>
-                  <div className={styles.push}>
-                    <div className={styles.good}>いいね</div>
-                    <div className={styles.money}>スパチャ</div>
-                  </div>
-                  <div className={styles.comment}>コメント</div>
-                  <button
-                    className={styles.reportButton}
-                    onClick={handleReport}
-                    style={inputStyle}
-                  >
-                    通報
-                  </button>
-                </div>
-              </div>
-            </div>
-
-            <div className={styles.white}>
-              <div className={styles.post}>
-                <div className={styles.picture}>画像</div>
-                <div className={styles.info}>
-                  <button
-                    className={styles.followButton}
-                    onClick={handleFollow}
-                    style={inputStyle}
-                  >
-                    フォロー
-                  </button>
-                  <div className={styles.push}>
-                    <div className={styles.good}>いいね</div>
-                    <div className={styles.money}>スパチャ</div>
-                  </div>
-                  <div className={styles.comment}>コメント</div>
-                  <button
-                    className={styles.reportButton}
-                    onClick={handleReport}
-                    style={inputStyle}
-                  >
-                    通報
-                  </button>
-                </div>
-              </div>
-            </div>
-            
-          </div>
-
-          
-          <div className={styles.advertisement}>
-          <img
-              src={sampleTopImg} // 広告サンプル
-              alt="sampleTopImg" // 代替テキスト
-          />
-          </div>
+          ))}
         </div>
 
+        <div className={styles.advertisement}>
+          <img
+            src={sampleTopImg} // 広告サンプル
+            alt="sampleTopImg" // 代替テキスト
+          />
+        </div>
       </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default TopPage;
